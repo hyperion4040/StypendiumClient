e6ca3343712ef8cc8f58e83eb61a5821dce71c82
hyperion4040 <hyperion1987@wp.pl>
2017-11-14 22:58:15 +0100

    Initial commit

---
+++ StypendiumClient/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StypendiumClient.Models;
+
+namespace StypendiumClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // ReSharper disable Mvc.ViewNotResolved 
+            return View( ); 
+            // ReSharper restore Mvc.ViewNotResolved 
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            // ReSharper disable Mvc.ViewNotResolved 
+            return View( ); 
+            // ReSharper restore Mvc.ViewNotResolved 
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            // ReSharper disable Mvc.ViewNotResolved 
+            return View( ); 
+            // ReSharper restore Mvc.ViewNotResolved 
+        }
+
+        public IActionResult Error()
+        {       // ReSharper disable Mvc.ViewNotResolved 
+
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+            // ReSharper restore Mvc.ViewNotResolved 
+        }
+        
+        public IActionResult GetPersons()
+        {
+            
+
+        }
+        
+        
+    }
+}
