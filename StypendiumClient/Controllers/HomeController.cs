7455bbe4a99a590cef2de8cabd9689bb76254618
hyperion4040 <hyperion1987@wp.pl>
2017-11-16 10:33:51 +0100

    Problem with deserialize single Person object resolved.

--- StypendiumClient/Controllers/HomeController.cs
+++ StypendiumClient/Controllers/HomeController.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using StypendiumClient.Models;
 
 namespace StypendiumClient.Controllers
 {
-    [Route("/api/[controller]")]
+//    [Route("/api/[controller]")]
     public class HomeController : Controller
     {
         //static HttpClient client = new HttpClient();
@@ -46,44 +50,56 @@ namespace StypendiumClient.Controllers
             // ReSharper restore Mvc.ViewNotResolved 
         }
         
-        /*public IActionResult Start()
-        {
-            
-            _httpClient.BaseAddress = new Uri();
-            
-
-        }*/
         
-        /*static async Task RunAsync()
-        {
-            // New code:
-            client.BaseAddress = new Uri("http://localhost:5050");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            
-        }*/
-        [HttpGet]
+        //[HttpGet]
         public string Get()
         {
             using (var client = new HttpClient())
             {
-                //client.BaseAddress = new Uri("http://localhost:5050");
-                client.BaseAddress = new Uri("http://stypendium.azurewebsites.net");
+                client.BaseAddress = new Uri("http://localhost:5050");
+//                client.BaseAddress = new Uri("http://stypendium.azurewebsites.net");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var response = client.GetAsync("api/person").Result;
+                var response = client.GetAsync("api/person/3").Result;
                 
                 if (response.IsSuccessStatusCode)
                 {
                     string responseString = response.Content.ReadAsStringAsync().Result;
                     
+                    
                     return responseString;
                 }
             }
+
             return "";
         }
+
+        public string metodaHelp()
+        {
+//            List<Person> persons = DeserializeAccounts(Get());
+//            int i = persons[1].Id;
+//            return i;
+           /* var  person = new Person(1,"Adrian");
+            var response = JsonConvert.SerializeObject(person);
+            Person wynik = JsonConvert.DeserializeObject<Person>(response);*/
+            var respons = Get();
+            List<Person> wynik = JsonConvert.DeserializeObject<List<Person>>(respons);
+            
+//            var response = JsonConvert.DeserializeObject<Person>(Get());
+           
+            return wynik[0].Name;
+
+        }
+        public List<Person> DeserializeAccounts(string json)
+        {
+            return JsonConvert.DeserializeObject<List<Person>>(json);
+        }
         
-        
+        public IActionResult start()
+        {
+            
+            return View("start");
+        }
         
        
     }
--- StypendiumClient/Models/Person.cs
+++ StypendiumClient/Models/Person.cs
@@ -1,8 +1,35 @@
-﻿namespace StypendiumClient.Models
+﻿using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace StypendiumClient.Models
 {
     public class Person
     {
+        public Person(string jsoName)
+        {
+            
+            
+            JObject jObject = JObject.Parse(jsoName);
+            JToken jUser = jObject["value"];
+            Id = (int) jUser.First;
+            Name = (string) jUser.Next;
+            /*Id = (int) jUser["id"];
+            Name = (string) jUser["name"];*/
+        }
+        [JsonConstructor]
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        [JsonProperty(PropertyName = "id")]
         public int Id { set; get; }
+        [JsonProperty(PropertyName = "name")]
         public string Name { set; get; }
     }
 }
